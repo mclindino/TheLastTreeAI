048217c22e018f047c1e580c48e65ae8f35f8455
andredallacosta <andrezdc02@gmail.com>
2019-12-08 23:22:02 -0300

    Add bad heuristics

--- AnBotDino.py
+++ AnBotDino.py
@@ -2,14 +2,37 @@ import urllib.request
 import sys
 import time
 from server import Game
-from minimax import MiniMax
+import random
 
 simulated_game = Game()
 simulated_game.init_board(2)
 
+def minimax2_0(tree, objetivo):
+    for children_uno in tree.getChildren():
+        _max = -10000
+        
+        if(len(children_uno.getChildren()) == 0):
+            return 
+        
+        for children_duno in children_uno.getChildren():
+            _min = 10000
+
+            if(len(children_duno.getChildren()) == 0):
+                return 
+
+            for children_trino in children_duno.getChildren():
+                children_trino.setScore(avaliation(objetivo, children_trino.getValue()))
+                if children_trino.getScore() < _min:
+                    _min = children_trino.getScore()
+            children_duno.setScore(_min)
+            if children_duno.getScore() > _max:
+                _max = children_duno.getScore()
+        children_uno.setScore(_max)
+
+
 class tree:
     def __init__(self, value):
-        self.score = 0
+        self.score = 2
         self.value = value
         self.children = []
 
@@ -30,19 +53,13 @@ class tree:
         return self.score
 
 def avaliation(objetivo, movimento):
-    rule = movimento.value[0]
-    animal = movimento.value[1]
-    land = movimento.value[2]
+    rule = movimento[0]
+    animal = movimento[1]
+    land = movimento[2]
 
     score = 0
 
     if objetivo == 10:
-        # best_animal = animal[0]
-        # for animal in animals:
-        #     if animal.fruits > best_animal.fruits:
-        #         best_animal = animal
-        # if land[best_animal.land].tree >= 1:
-
         score += simulated_game.animals[animal].fruits * 2
         if simulated_game.lands[land].trees >= 1:
             score += 2
@@ -60,7 +77,7 @@ def avaliation(objetivo, movimento):
     
     elif objetivo == 11:
         if simulated_game.lands[land].trees > 0:
-            if animal.fruits > 0:
+            if simulated_game.animals[animal].fruits > 0:
                 if rule == 0:
                     score += 2
                 else:
@@ -102,8 +119,66 @@ def avaliation(objetivo, movimento):
 
         return score
     
-    #elif objetivo == 13:
+    elif objetivo == 13:
+        if rule == 1:
+            for animal1 in simulated_game.animals:
+                if simulated_game.lands[animal1.land].trees == (animal1.fruits + 1):
+                    score += 3
+                else:
+                    score += 1
+            
+        if rule == 4:
+            for land1 in simulated_game.lands:
+                if simulated_game.animals[animal].fruits == (land1.trees + 1):
+                    score += 3
+                else:
+                    score += 1
+        return score
 
+    elif objetivo == 14:
+        quantityTrees = simulated_game.lands[land].trees
+        quantityFruits = simulated_game.animals[animal].fruits
+        if quantityTrees == 0:
+            score += 2
+        if quantityFruits == 0:
+            score += 2
+        if simulated_game.lands[land - 1].trees:
+            if simulated_game.lands[land + 1].trees:
+                if simulated_game.lands[land - 1].trees == simulated_game.lands[land + 1].trees:
+                    score += 3
+        if (rule != 5) or (rule != 1):
+            score += 1
+        return score
+        
+    elif objetivo == 15:
+        if rule == 2:
+            if simulated_game.lands[land].seeds <= 0:
+                score += 3
+            else:
+                score += 1
+        if rule != 3:
+            score += 2
+        return score
+
+    elif objetivo == 16:
+        currentFruits = simulated_game.animals[animal].fruits
+        if rule == 1:
+            for animal1 in simulated_game.animals:
+                if (currentFruits + 1) != animal1.fruits:
+                    score += 2
+                else:
+                    score += 1
+        return score
+
+    elif objetivo == 17:
+        trees = simulated_game.lands[land].trees
+        if trees >= 4:
+            score += 3
+        for animal1 in simulated_game.animals:
+            if land == animal1.land:
+                score += 2
+        return score
+        
 if len(sys.argv)==1:
     print("Voce deve especificar o numero do jogador (0 ou 1)\n\nExemplo:    ./random_client.py 0")
     quit()
@@ -145,6 +220,9 @@ while not done:
             land = int(possible_moviments.value[2])
             simulated_game.make_move(player_turn, rule, animal, land)
             movimentos = simulated_game.get_available_moves(opponent)
+            
+            if(len(movimentos) == 0):
+                break
 
             possible_moviments.setChildren(movimentos)
 
@@ -155,27 +233,38 @@ while not done:
                 simulated_game.make_move(player_turn, rule, animal, land)
                 
                 movimentos = simulated_game.get_available_moves(player_turn)
+                
+                if(len(movimentos) == 0):
+                    break
+                
                 possible_moviments_le_two.setChildren(movimentos)
 
-                for possible_moviments_le_three in possible_moviments_le_two.getChildren():
-                    possible_moviments_le_three.setScore(avaliation(objetivo, possible_moviments_le_three))
-        
-        minimax_try = MiniMax(megaTree)
-        minimax_try.minimax(megaTree)
-        # print(megaTree.value)
-        # for i in megaTree.getChildren():
-        #     print('\t' + str(i.value))
-        #     for j in i.getChildren():
-        #         print('\t\t' + str(j.value))
-        #         for k in j.getChildren():
-        #             print('\t\t\t' + str(k.value))
+        minimax2_0(megaTree, objetivo)
 
-        # Escolhe um movimento aleatoriamente
-        #movimento = random.choice(movimentos)
-        
+        melhor = -1
+        for elemento in megaTree.getChildren():
+            if elemento.getScore() > melhor:
+                melhor = elemento.getScore()
+
+        list_melhores = []
+        for i in megaTree.getChildren():
+            for j in i.getChildren():
+                for k in j.getChildren():
+                    if k.getScore() == melhor:
+                        list_melhores.append(k.getValue())
+
+        resp = urllib.request.urlopen("%s/movimentos?player=%d" % (host,player))
+        movimentos = eval(resp.read())
+        conseguiu = False
+        while(not conseguiu):
+            movimento_escolhido = list_melhores[random.randrange(0, (len(list_melhores)-1))]
+            if movimento_escolhido in movimentos:
+                conseguiu = True
+
+        movimento = movimento_escolhido
         # Executa o movimento
-        #resp = urllib.request.urlopen("%s/move?player=%d&rule=%d&animal=%d&land=%d" % (host,player,movimento[0],movimento[1],movimento[2]))
-        #msg = eval(resp.read())
+        resp = urllib.request.urlopen("%s/move?player=%d&rule=%d&animal=%d&land=%d" % (host,player,movimento[0],movimento[1],movimento[2]))
+        msg = eval(resp.read())
         
         # Se com o movimento o jogo acabou, o cliente venceu
         if msg[0]==0:
